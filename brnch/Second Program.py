fae4e06b67e606cb48d3de23c9deb3ce9ea7ecb4
ssanthu49 <ssanthu49@gmail.com>
2020-04-29 08:55:57 +0530

    brnch

---
+++ brnch/Second Program.py
@@ -0,0 +1,19 @@
+while True:
+    age=int(input('Enter your age: '))
+    if age>=0 and age<=10:
+        print("You Are A Child")
+    elif age>20 and age<=30:
+        print("YOU ARE A Bachelour")
+    elif age>10 and age<=20:
+        print("You Are A teenage")
+    elif age>30 and age<=50:
+        print("You Are A Uncle")
+    elif age>50 and age<=100:
+        print("You Are A Grandpa")
+    else :
+        B=2
+        b=str(B)
+        print("count"+b)
+        print("You Are A GOD")
+    
+print("Enter to Exit")
